850939247e1a12159603b2ddde97e3363e27e37b
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-09-24 07:52:52 +0100

    Updated the autostart.py file a bit

--- autostart.py
+++ autostart.py
@@ -1,3 +1,3 @@
 import subprocess
-subprocess.call ('python3 bot.py')
-subprocess.call ('java -Dnogui=true -jar JMusicBot-0.2.5-Linux.jar')
+print('Starting the bot...')
+subprocess.call ('python3 bot.py && java -Dnogui=true -jar JMusicBot-0.2.5-Linux.jar')
