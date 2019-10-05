1827e1d4e86179e2c37a54761073081436c5ee9c
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-10-05 15:50:28 +0100

    The status changer

---
+++ status.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import asyncio 
+#Modules that are going to be imported
+
+client = commands.Bot(command_prefix='/')
+with open("token.txt") as f:
+    token = f.read()
+    f.close()
+#Variables above
+
+@client.event
+async def on_ready():
+    while True:
+
+        activity = discord.Activity(name='Nightcore', type=discord.ActivityType.listening)
+        await client.change_presence(activity=activity)
+        await asyncio.sleep(12)
+
+        activity = discord.Activity(name='Streaming bootiful music to everyone', type=discord.ActivityType.streaming)
+        await client.change_presence(activity=activity)
+        await asyncio.sleep(12)
+
+        activity = discord.Activity(name='on ManjaroPE (Best Mcpe Server)', type=discord.ActivityType.playing)
+        await client.change_presence(activity=activity)
+        await asyncio.sleep(12)
+
+        activity = discord.Activity(name='/help to people using this bot', type=discord.ActivityType.streaming)
+        await client.change_presence(activity=activity)
+        await asyncio.sleep(12)
+#Changes the status every 12 seconds
+
+client.run(token)
