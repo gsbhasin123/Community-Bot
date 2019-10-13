0a949f15c729d660f9391efc7b1d1bf72ddd64f9
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-10-13 20:13:35 +0100

    Had to remove the cross link and make it a separate file

--- bot.py
+++ bot.py
@@ -34,32 +34,6 @@ async def on_ready():
 async def server_count(ctx):
         await ctx.send(len(bot.guilds))
 
-@bot.listen()
-
-async def on_message(message):
-    CIDs = [632912136787591168,632912159793348618,632912187626618880,632929328652484609]
-    msg = message.content
-    msg = msg.replace("@", "(a)")
-    X = 0
-    
-    try:
-
-        if message.author.id != bot.user.id:
-    	        
-            if message.channel.id in CIDs:
-        	
-                CIDs.remove(message.channel.id)
-            
-                for channel in CIDs:
-                    await bot.get_channel(CIDs[X]).send(f'{message.author}: {msg}')
-                    X = X + 1
-                CIDs.append[message.channel.id]
-            
-            else:
-                pass     
-    except:
-        pass
-
 @bot.command()
 async def github(ctx):
     embed=discord.Embed(title="GitHub", description="Thanks you for being interested in Community bot! If you want to commit please know we don't mind whatever language you choose to help us(Even `C`)\nhttps://github.com/IpProxyNeon/Community-discord-bot/blob/master/README.md", color=0x00ffff)
