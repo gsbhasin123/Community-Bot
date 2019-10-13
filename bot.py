22e9b76d2024b3bb2a007d4c02714ddcdcc8f2f2
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-10-13 18:17:28 +0100

    Added a multi-server chat feature. Just leave as is if you don't want it.

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def on_ready():
         await bot.change_presence(activity=activity)
         await asyncio.sleep(12)
 
-        activity = discord.Activity(name='bootiful music to everyone', type=discord.ActivityType.streaming)
+        activity = discord.Activity(name=(f'{len(bot.guilds)} Servers'), type=discord.ActivityType.watching)
         await bot.change_presence(activity=activity)
         await asyncio.sleep(12)
 
@@ -38,5 +38,31 @@ async def server_count(ctx):
 async def github(ctx):
     embed=discord.Embed(title="GitHub", description="Thanks you for being interested in Community bot! If you want to commit please know we don't mind whatever language you choose to help us(Even `C`)\nhttps://github.com/IpProxyNeon/Community-discord-bot/blob/master/README.md", color=0x00ffff)
     await ctx.send(embed=embed)
+ 
+#If you want to disable cross link delete from
+#Here to:   
+@bot.event
+
+async def on_message(message):
+    CIDs = [632866275957407764,632857499535671301,610218900021313553,610218548920582155,632884808795815936,632872557548404738,632989220960600082]
+    #Replace the IDs with your channel IDs if you want cross link.
+    msg = message.content
+    msg = msg.replace("@", "(a)")
+    X = 0
+    
+    if message.author.id != bot.user.id:
+    	
+        if message.channel.id in CIDs:
+        	
+            CIDs.remove(message.channel.id)
+            
+            for channel in CIDs:
+                await bot.get_channel(CIDs[X]).send(f'{message.author}: {msg}')
+                X = X + 1
+            CIDs.append[message.channel.id]
+            
+        else:
+            pass     
+#Here
 
 bot.run(token)
