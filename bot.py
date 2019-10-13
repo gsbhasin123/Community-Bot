0e035c30e1db2950075acb2bfc07b52b44973bcf
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-10-13 19:48:31 +0100

    Having an event just completely destroyed the commands, all fixed now!

--- bot.py
+++ bot.py
@@ -34,35 +34,35 @@ async def on_ready():
 async def server_count(ctx):
         await ctx.send(len(bot.guilds))
 
-@bot.command()
-async def github(ctx):
-    embed=discord.Embed(title="GitHub", description="Thanks you for being interested in Community bot! If you want to commit please know we don't mind whatever language you choose to help us(Even `C`)\nhttps://github.com/IpProxyNeon/Community-discord-bot/blob/master/README.md", color=0x00ffff)
-    await ctx.send(embed=embed)
- 
-#If you want to disable cross link delete from
-#Here to:   
-@bot.event
+@bot.listen()
 
 async def on_message(message):
-    CIDs = [632866275957407764,632857499535671301,610218900021313553,610218548920582155,632884808795815936,632872557548404738,632989220960600082]
-    #Replace the IDs with your channel IDs if you want cross link.
+    CIDs = [632912136787591168,632912159793348618,632912187626618880,632929328652484609]
     msg = message.content
     msg = msg.replace("@", "(a)")
     X = 0
     
-    if message.author.id != bot.user.id:
-    	
-        if message.channel.id in CIDs:
+    try:
+
+        if message.author.id != bot.user.id:
+    	        
+            if message.channel.id in CIDs:
         	
-            CIDs.remove(message.channel.id)
+                CIDs.remove(message.channel.id)
             
-            for channel in CIDs:
-                await bot.get_channel(CIDs[X]).send(f'{message.author}: {msg}')
-                X = X + 1
-            CIDs.append[message.channel.id]
+                for channel in CIDs:
+                    await bot.get_channel(CIDs[X]).send(f'{message.author}: {msg}')
+                    X = X + 1
+                CIDs.append[message.channel.id]
             
-        else:
-            pass     
-#Here
+            else:
+                pass     
+    except:
+        pass
+
+@bot.command()
+async def github(ctx):
+    embed=discord.Embed(title="GitHub", description="Thanks you for being interested in Community bot! If you want to commit please know we don't mind whatever language you choose to help us(Even `C`)\nhttps://github.com/IpProxyNeon/Community-discord-bot/blob/master/README.md", color=0x00ffff)
+    await ctx.send(embed=embed)
 
 bot.run(token)
