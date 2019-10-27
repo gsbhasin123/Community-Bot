96aef150a7b848fe4f881272b236f27dbab15b65
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-10-27 19:21:11 +0000

    Made a few changes here and there

--- bot.py
+++ bot.py
@@ -6,20 +6,32 @@ with open('token.txt') as f:
     token = f.read()
 
 bot = commands.Bot(command_prefix='/')
-OIDs = [524288464422830095,241694485694775296,624305005385482281]
+OIDs = [524288464422830095,241694485694775296,624305005385482281,401430005055488011]
 
-@bot.command(name='exit()')
-async def stop(ctx):
-    if ctx.author.id in OIDs:
-        await ctx.send("Stopping bot...")
-        exit()
-    else:
-        pass
+@bot.listen()
+async def on_ready():
+    while True:
+        activity = discord.Activity(name='Nightcore', type=discord.ActivityType.listening)
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(12)
+        activity = discord.Activity(name=(f'{len(bot.guilds)} Servers'), type=discord.ActivityType.watching)
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(12)
+        activity = discord.Activity(name='on ManjaroPE (Best Mcpe Server)', type=discord.ActivityType.playing)
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(12)
+        activity = discord.Activity(name='/help to people using this bot', type=discord.ActivityType.streaming)
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(12)
 
 @bot.command()
 async def load(ctx, extension):
-    await ctx.send(f"Loading {extension}...")
     if ctx.author.id in OIDs:
+        if extension == 'system':
+            await ctx.send("***Warning this is a dangerous cog, it can interact with your systems Terminal directly, please take caution***")
+        else:
+            pass
+        await ctx.send(f"Loading {extension}...")
         bot.load_extension(f'cogs.{extension}')
         await ctx.send(f"{extension} has been loaded!")
     else:
@@ -40,6 +52,11 @@ async def reload(ctx, extension):
 @bot.command()
 async def restart(ctx):
     if ctx.author.id in OIDs:
+        try:
+            bot.load_extension('cogs.system')
+            print("Unloading the system cog now... (Needed to restart the bot)")
+        except:
+            pass
         await ctx.send("Restarting the bot now...")
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
@@ -48,6 +65,10 @@ async def restart(ctx):
             else:
                 pass
         await ctx.send("Done!")
+        try:
+            bot.unload_extension('cogs.system')
+        except:
+            pass
     else:
         await ctx.send("You are not allowed to do this!")
 
@@ -60,7 +81,7 @@ async def cog_list(ctx):
             X = X + 1
         else:
             pass
-    await ctx.send(f"In total there are {X} cogs")
+    await ctx.send(f"In total there are {X} cogs (Some may be disabled e.g system might be disabled for the bot host's safety)")
 
 @bot.command()
 async def unload(ctx, extension):
@@ -68,13 +89,19 @@ async def unload(ctx, extension):
         await ctx.send(f"Unloading {extension}...")
         bot.unload_extension(f'cogs.{extension}')
         await ctx.send(f"{extension} has been unloaded!")
-        await ctx.send(f"The cog '{extension}' isn't even loaded!")
+    else:
+        pass
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         print(f"Loading {filename}")
         bot.load_extension(f'cogs.{filename[:-3]}')
         print(f"Loaded {filename}")
+    try:
+        bot.unload_extension('cogs.system')
+        print("Unloaded system.py")
+    except:
+        pass
     else:
         pass
 
