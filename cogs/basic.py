edb8141c192163abdc84e42392c5d4588c1bbd52
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-10-27 13:56:27 +0000

    Added the first cog!

---
+++ cogs/basic.py
@@ -0,0 +1,23 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+class basic(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+        print("'basic' Cog has been loaded!")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"Bot has logged in as {self.bot.user}")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error,commands.CommandNotFound):
+            pass
+        else:
+            print(error)
+            await ctx.send(error)
+
+def setup(bot):
+    bot.add_cog(basic(bot))
