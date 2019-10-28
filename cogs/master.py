5d87c81311ad3e92e6625e2cfe684ac3b7a8f69c
Xevion <xevioni@yandex.com>
2019-10-28 18:52:13 -0500

    refactored master.py for use with new config.py

--- cogs/master.py
+++ cogs/master.py
@@ -1,11 +1,8 @@
 import json
+import config
 import asyncio
 import discord
 from discord.ext import commands
-
-f=open('masters.json','r+')
-masters = json.load(f)
-f.close()
 
 class Master(commands.Cog):
     def __init__(self, bot):
@@ -14,25 +11,22 @@ class Master(commands.Cog):
 
     @commands.command()
     async def say(self, ctx, *, msg):
-        if ctx.author.id in masters:
+        if config.MASTERS.contains(ctx.author.id):
             await ctx.message.delete()
             await ctx.send(msg)
         else:
-            await ctx.send("You don't own meh")
+            await ctx.send("You are not a master.")
 
     @commands.command()
     async def spam(self, ctx, *, msg):
-        if ctx.author.id in masters:
+        if config.MASTERS.contains(ctx.author.id):
             for s in range(0, 100):
-                if ctx.author.id in masters:
-                    if ctx.message.content == "stop":
+                if config.MASTERS.contains(ctx.author.id) and ctx.message.content == "stop":
                         break
-                    else:
-                        await ctx.send(msg)
                 else:
-                    pass
+                    await ctx.send(msg)
         else:
-            await ctx.send("No I'm not gonna spam for you! Screw you!")
+            await ctx.send("You are not a Master.")
 
 
 def setup(bot):
