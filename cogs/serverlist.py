a73549e5410ff4468039b3353680d0cb9b55e898
Xevion <xevioni@yandex.com>
2019-10-28 00:23:55 -0500

    changes to wording

--- cogs/serverlist.py
+++ cogs/serverlist.py
@@ -12,17 +12,17 @@ class ServerList(commands.Cog):
     #Sends a list of the current servers its in in the current channel
     @commands.command(name='server-list')
     async def server_list(self,ctx):
-        await ctx.send("Warning, it's a little spammy and takes a while to finish")
+        await ctx.send("Warning - this command may take a moment...")
         await ctx.send('\n'.join(f'- {guild.name}' for guild in self.bot.guilds))
 
     #Sends the list of servers to the server list channel
     @commands.Cog.listener()
     async def on_guild_join(self,guild):
-        await bot.get_channel(CENTRAL_CHANNEL).send(f'- {guild.name} has joined the Bot!')
+        await bot.get_channel(CENTRAL_CHANNEL).send(f'- "{guild.name}" has joined the Bot!')
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        await bot.get_channel(CENTRAL_CHANNEL).send(f'- {guild.name} has left the Bot!')
+        await bot.get_channel(CENTRAL_CHANNEL).send(f'- "{guild.name}" has left the Bot!')
 
 def setup(bot):
     bot.add_cog(ServerList(bot))
