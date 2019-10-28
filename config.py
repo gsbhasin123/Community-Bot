441d7f9869c9944b7d82f3dd90f60cba5805de5e
Xevion <xevioni@yandex.com>
2019-10-28 16:48:12 -0500

    work to quickly validate file in orderly manner

--- config.py
+++ config.py
@@ -2,12 +2,30 @@ import os
 import sys
 import json
 import asyncio
+import logging
 
-CONFIG_PATH = os.path.join(sys.path[0], 'config')
+
+# All required files for the config to be in order
+# First value is the suggested type (None if complex)
+# Second value is the default value, could be list or dict or other JSON-seriable object
 FILES = {
-    'banned-commands.json' : {},
-    'crosslink-ids.json' : {},
-    'masters.json' : {},
-    'o-ids.json' : {},
-    'sp-ids.json' : {}
+    'banned-commands.json' : (str, []),
+    'crosslink-ids.json' : (int, []),
+    'masters.json' : (int, []),
+    'o-ids.json' : (int, []),
+    'sp-ids.json' : (int, [])
 }
+
+# Constants
+ALL_SAME_TYPE = lambda array, item_type : all(type(item) is item_type for item in array) # Lambda for detecting that all items in an array are of type 'item_type'
+ROOT = sys.path[0]
+CONFIG_PATH = os.path.join(ROOT, 'config')
+AVAILABLE_CONFIGURATION_FILES = os.listdir(CONFIG_PATH)
+logging.basicConfig(level=logging.INFO)
+
+# Start validating all required files
+for file in FILES.keys():
+    if file in AVAILABLE_CONFIGURATION_FILES:
+        with open(os.path.join(CONFIG_PATH, file), 'r') as file:
+                file = json.load(file.read())
+                logging.warning(e)
