40700bd5602432a7f0d6a8fafd30cd49f03268e6
Xevion <xevioni@yandex.com>
2019-10-28 17:21:14 -0500

    finished with config file validation

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import sys
 import json
 import asyncio
 import logging
+import functools
 
 
 # All required files for the config to be in order
@@ -24,8 +25,37 @@ AVAILABLE_CONFIGURATION_FILES = os.listdir(CONFIG_PATH)
 logging.basicConfig(level=logging.INFO)
 
 # Start validating all required files
+logging.info(f'Starting validation of {len(FILES.keys())} configuration files.')
 for file in FILES.keys():
     if file in AVAILABLE_CONFIGURATION_FILES:
-        with open(os.path.join(CONFIG_PATH, file), 'r') as file:
-                file = json.load(file.read())
-                logging.warning(e)
+        with open(os.path.join(CONFIG_PATH, file), 'r') as fp:
+            # Open and validate that the file is valid json
+            try:
+                fp = json.load(fp)
+            except OSError:
+                logging.critical(f'Configuration file \'{file}\' could not be opened. Please close any programs that may be preventing access to the file.')
+                break
+            except json.JSONDecodeError: # Incorrect JSON syntax possibly 
+                logging.critical(f'Configuration file \'{file}\' could not be deserialized.')
+                logging.critical(f'Please check that the configuration file\'s syntax is intact.')
+                break
+            # Validate that the file is what we want out of the file
+            if FILES[file][0] is not None: # if a type was given
+                check_func = functools.partial(ALL_SAME_TYPE, item_type=FILES[file][0]) # create the checking func with the type we want
+                if not check_func(fp):
+                    logging.warning(f'Configuration file \'{file}\' did not pass tests for all values being of type \'{FILES[file][0]}\'.') # may want to make this critical?
+                else:
+                    logging.debug(f'Configuration file \'{file}\' passed similar types test.')
+            # All good.
+            logging.info(f'Successfully read \'{file}\' configuration file.')
+    else:
+        # No configuration file was found -> Create a new one using provided default values
+        logging.warning(f'Configuration file \'{file}\' could not be found.')
+        path = os.path.join(CONFIG_PATH, file)
+        shortpath = f'./config/{file}'
+        # Write default value to file
+        with open(path, 'w+') as fp:
+            json.dump(FILES[file][1], fp)
+        # Close with
+        logging.debug(f'Successfully wrote configuration file at \'{shortpath}\'')
+        logging.info(f'Configuration file will be read with default values and a new configuration file has been placed at \'{shortpath}\'.')
