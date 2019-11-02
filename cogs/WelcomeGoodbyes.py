555776e33cdc0c7750b05a42ecbc104c5fd1a970
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-11-02 19:53:26 +0000

    Updated the channels for the welcomer/leaver

--- cogs/WelcomeGoodbyes.py
+++ cogs/WelcomeGoodbyes.py
@@ -12,17 +12,15 @@ class WelcomeGoodbyes(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self,member):
-        if member.guild.id == 612844992267943937:
-            role = member.guild.get_role(612900235185881088)
-            await member.add_roles(role)
-            await self.bot.get_channel(612893762770960384).send(f"<@{member.id}> has joined the server! Thanks for joining!")
+        if member.guild.id == 637899212385812491:
+            await self.bot.get_channel(640200978225954846).send(f"<@{member.id}> has joined the server! Thanks for joining!")
         else:
             pass
 
     @commands.Cog.listener()
     async def on_member_remove(self,member):
-        if member.guild.id == 612844992267943937:
-            await self.bot.get_channel(612893762770960384).send(f"{member} has left the server... We hope that you will come back soon...")
+        if member.guild.id == 637899212385812491:
+            await self.bot.get_channel(640200978225954846).send(f"{member} has left the server... We hope that you will come back soon...")
         else:
             pass
 
