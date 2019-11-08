11014e599ad6725d75698d5f6ee4de5be978a24e
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-11-08 07:17:38 +0000

    Added a clear/purge command

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -34,6 +34,21 @@ class moderation(commands.Cog):
 
         await member.kick(reason=reason)
 
+    @commands.command(name="clear", alias="purge")
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount=1):
+        """
+        Clear specific amount of messages in this channel
+        """
+        if amount < 0:
+            await ctx.send("I can only delete positive amount of messages!")
+            amount = 0
+            
+        await ctx.trigger_typing()
+        await ctx.channel.purge(limit=amount)
+        embed = discord.Embed(title="Cleared messages", description=f"{amount} message(s) cleared\nSelf destructing after 3 seconds...", color=0x00ff00)
+        await ctx.send(embed=embed,delete_after=float(3))
+
 def setup(bot):
 
     bot.add_cog(moderation(bot))
