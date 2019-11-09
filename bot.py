46bd9ac1f9bb7b787bd5fde22e6454a146b55d2c
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-11-09 20:00:51 +0000

    Just some things I forgot

--- bot.py
+++ bot.py
@@ -14,7 +14,31 @@ discordLogging.setLevel(logging.WARNING)
 
 #Both of these are needed so mine and AUser's
 #cogs work on eachother's bot as he uses 'client' and
-#I use 'bot'
+#I use 'bot' and he uses 'client'
+
+@bot.command(name='command-cog-update')
+async def update_command_cog(ctx):
+    await ctx.send("Updating...")
+    bot.unload_extension("cogs.commands-with-cogs")
+    subprocess.call("wget -O commands-with-cogs.py https://raw.githubusercontent.com/IpProxyNeon/Community-discord-bot/master/cogs/commands-with-cogs.py",shell=True)
+    bot.load_extension("cogs.commands-with-cogs")
+    await ctx.send("Done")
+
+@bot.listen()
+async def on_ready():
+    while True:
+        activity = discord.Activity(name='Nightcore', type=discord.ActivityType.listening)
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(12)
+        activity = discord.Activity(name=(f'{len(bot.guilds)} Servers'), type=discord.ActivityType.watching)
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(12)
+        activity = discord.Activity(name='on ManjaroPE (Best MCPE Server)', type=discord.ActivityType.playing)
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(12)
+        activity = discord.Activity(name='/help to people using this bot', type=discord.ActivityType.streaming)
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(12)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
