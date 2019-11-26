448491ade65740ebdca0cd2d3120faa24d73754b
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-11-26 16:30:05 +0000

    Made it actually Owner Only and removed unneeded clutter

--- cogs/help.py
+++ cogs/help.py
@@ -11,36 +11,32 @@ class Help(commands.Cog):
             
     @commands.command(name="help")
     async def help(self, ctx):
-        if config.OWNER_IDS.contains(ctx.author.id):
-            help=open("help.txt","r")
-            embed = discord.Embed(
-            title="Help",
-            description=f"Note: Commands are not case sensitive\n{help.read()}\n====Bot owner's help====\n\n/load (cog) - Does what it says\n\n/unload (cog) - Unloads a cog\n\n/reload (cog) - Reloads a cog\n\n/restart - Reloads all cogs\n\n/add-cog (name of cog) (GitHub raw of cog) - Downloads a cog from the internet (From GitHub, you need to use raw) the loads it\n\n/remove-cog (cog name) - Unloads the cog, then deletes it\n\n/update-cog (cog name) (GitHub raw of cog) - Updates the cog, then reloads it",
-            color=0x21FFAF,
-            )
-            await ctx.send(embed=embed)
-            help.close()
-        else:
-            help=open("help.txt","r")
-            embed = discord.Embed(
-            title="Help",
-            description=f"Note: Commands are not case sensitive\n{help.read()}",
-            color=0x21FFAF,
-            )
-            await ctx.send(embed=embed)
-            help.close()
+        help=open("help.txt","r")
+        embed = discord.Embed(
+        title="Help",
+        description=f"Note: Commands are not case sensitive\n{help.read()}",
+        color=0x21FFAF,
+        )
+        await ctx.send(embed=embed)
+        help.close()
 
     @commands.command(name='owner-help')
     async def owner_help(self, ctx):
+        if config.OWNER_IDS.contains(ctx.author.id):
+            pass
+        else:
+            return
         Commands=''
+        X = 0
         for command in self.bot.commands:
             Commands = Commands + command.name + "\n"
-            embed = discord.Embed(
-            title="Help",
-            description=f"```css\n{Commands}```",
-            color=0x21FFAF,
-            )
-            await ctx.send(embed=embed)
+            X = X + 1
+        embed = discord.Embed(
+        title="Help",
+        description=f"```css\n{Commands}```There are {X} commands in total (Python bot only)",
+        color=0x21FFAF,
+        )
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Help(bot))
