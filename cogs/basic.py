5eac35eb1fbf81ba76a753fdbc38d82de922439e
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-11-26 17:50:45 +0000

    Had to add some IDs

--- cogs/basic.py
+++ cogs/basic.py
@@ -19,7 +19,7 @@ class Basic(commands.Cog):
         if message.author.id == 527431454356144129 and message.channel.id == 642725361192534029 and stuff[0] == 'CustomLinkCommand':
             msg = message.content.replace(stuff[0],' ')
             msg = msg.replace(stuff[1],' ')
-            banned_words = ['@everyone','@here','<@524288464422830095>']
+            banned_words = ['@everyone','@here','<@524288464422830095>','<@&614814593784610846>','<@&614814354101108737>','<@&630103616384008192>','<@&630101051680489482>','<@&630100972173131793>','<@&630100906519691299>','<@&619937218244509696>','<@&619932108491718675>','<@&619931948395266068>','<@&624237682834538506>']
             for no in banned_words:
                 msg = msg.replace(no,'(This is bad)')
             await self.bot.get_channel(int(stuff[1])).send(msg)
