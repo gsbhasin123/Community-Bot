8217f82b98a6a02fef70fb75b1e70112e98c534f
IpProxyNeon <proxneon960@gmail.com>
2019-12-06 13:23:49 +0000

    Some changes to fix the bot in bash repl

--- start.py
+++ start.py
@@ -1,3 +1,5 @@
 from os import system as bash
-bash('pip3 install -r requirements.txt')
+modules = ['flask','sqlalchemy','youtube-dl','leonardo-team','utils','xiangcheck']
+for module in modules:
+    bash(f'pip3 install {module}')
 bash('python3 keepalive.py & python3 bot.py')
