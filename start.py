1c5ead4e1b6312c49c233da09e8f6331a510477b
IpProxyNeon <proxneon960@gmail.com>
2019-12-08 14:34:46 +0000

    A

--- start.py
+++
@@ -1,5 +0,0 @@
-from os import system as bash
-modules = ['flask','sqlalchemy','youtube-dl','leonardo-team','utils','xiangcheck']
-for module in modules:
-    bash(f'pip3 install {module}')
-bash('python3 keepalive.py & python3 bot.py')
