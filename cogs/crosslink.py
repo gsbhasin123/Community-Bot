53080a717d045b2dcdfa2a8ed30c3075f00ad035
HuyaneMatsu <37266847+HuyaneMatsu@users.noreply.github.com>
2019-12-18 21:44:10 +0100

    Update crosslink.py to new hata version
    Events added like: `client.events.message_create.append` now get the client too as first argument.

--- cogs/crosslink.py
+++ cogs/crosslink.py
@@ -17,7 +17,7 @@ async def add_link(client, message, content):
     except ValueError:
         content = 'Channel is already in the CrossLink network!'
     else:
-        client.events.message_create.append(Crossposter(client),message.channel)
+        client.events.message_create.append(crossposter,message.channel)
         content = f'Added {message.channel:m} to CrossLink Network!'
 
     await client.message_create(message.channel, content)
@@ -30,7 +30,7 @@ async def remove_link(client, message, content):
     except ValueError:
         content = 'Channel isn\'t in the CrossLink network.'
     else:
-        client.events.message_create.remove(Crossposter(client),message.channel)
+        client.events.message_create.remove(crossposter,message.channel)
         content = f'Removed {message.channel:m} from CrossLink Network'
 
     await client.message_create(message.channel, content)
@@ -45,74 +45,60 @@ async def get_links(client, message, content):
 
     await client.message_create(message.channel, content)
 
-class Crossposter(object):
-    __slots__ = ('client', )
-    
-    def __init__(self, client):
-        self.client = client
-
-    async def __call__(self, message):
-        client = self.client
-        
-        if (message.author is self.client) and (message.channel is UNFILTERED_CHANNEL):
-            channels = []
-            for channel_id in config.CROSSLINK_IDS:
-                channel = CHANNELS[channel_id]
-                if channel is UNFILTERED_CHANNEL:
-                    continue
-                
-                channels.append(channel)
-
-            if not channels:
-                return
-            
-            content = message.clean_content
-            if not content:
-                return
-
-            for channel in channels:
-                client.loop.create_task(client.message_create(channel, content))
-                        
-            return
-
-        if message.author.is_bot:
-            return
-
-        if STOP_CROSS_RP.fullmatch(message.content):
-            return
-
-        channels=[]
-        ignore_id=message.channel.id
+async def crossposter(client, message):
+    if (message.author is client) and (message.channel is UNFILTERED_CHANNEL):
+        channels = []
         for channel_id in config.CROSSLINK_IDS:
-            if channel_id==ignore_id:
+            channel = CHANNELS[channel_id]
+            if channel is UNFILTERED_CHANNEL:
                 continue
             
-            channel = CHANNELS[channel_id]
             channels.append(channel)
 
         if not channels:
             return
-
-        content=message.clean_content
+        
+        content = message.clean_content
         if not content:
             return
-        
-        content=f'**{message.guild.name}**-{message.author:f}:\n{content}'
 
         for channel in channels:
             client.loop.create_task(client.message_create(channel, content))
-        
-    def __eq__(self,other):
-        if type(self) is not type(other):
-            return NotImplemented
+                    
+        return
 
-        return (self.client is other.client)
+    if message.author.is_bot:
+        return
+
+    if STOP_CROSS_RP.fullmatch(message.content):
+        return
+
+    channels=[]
+    ignore_id=message.channel.id
+    for channel_id in config.CROSSLINK_IDS:
+        if channel_id==ignore_id:
+            continue
+        
+        channel = CHANNELS[channel_id]
+        channels.append(channel)
+
+    if not channels:
+        return
+
+    content=message.clean_content
+    if not content:
+        return
+    
+    content=f'**{message.guild.name}**-{message.author:f}:\n{content}'
+
+    for channel in channels:
+        client.loop.create_task(client.message_create(channel, content))
 
 async def channel_delete(client, channel):
     if channel.id not in config.CROSSLINK_IDS:
         return
     
-    client.events.message_create.remove(Crossposter(client),channel)
+    client.events.message_create.remove(crossposter,channel)
     
     await config.CROSSLINK_IDS.remove(channel.id)
     
@@ -135,12 +121,11 @@ async def entry(client):
     
     if to_delete:
         await config.CROSSLINK_IDS.remove_multiple(to_delete)
-        
-    CROSSPOSTER = Crossposter(client)
+    
     event=client.events.message_create
     
     for channel in channels:
-        event.append(CROSSPOSTER, channel)
+        event.append(crossposter, channel)
             
 
 def exit(client):
@@ -149,7 +134,6 @@ def exit(client):
     if not config.CROSSLINK_IDS:
         return
 
-    CROSSPOSTER = Crossposter(client)
     event=client.events.message_create
     
     channels=[]
@@ -158,7 +142,4 @@ def exit(client):
         channels.append(channel)
     
     for channel in channels:
-        event.remove(CROSSPOSTER, channel)
-
-
-
+        event.remove(crossposter, channel)
