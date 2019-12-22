db5c1bd9fe4d2e22433df8882da2f7082a1e8b92
IpProxyNeon <proxneon960@gmail.com>
2019-12-22 08:33:40 +0000

    oof

--- start.py
+++ start.py
@@ -1,5 +1,8 @@
 from os import system as bash
+import threading
 bash('pip3 install -r reqs.txt')
-bash('ruby ext/setup.rb')
-bash('ruby ext/addons/mail.rb ext/data/mail/spool & python3 bot.py & python3 keepalive.py')
+def ruby():
+    bash('ruby ./ext/ruby/setup.rb')
+threading.Thread(target=ruby)
+bash('python3 bot.py & python3 keepalive.py')
 
