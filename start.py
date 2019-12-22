cf1cc673ce2b34a839804ddfce56659ca7b88c37
IpProxyNeon <55587033+IpProxyNeon@users.noreply.github.com>
2019-12-22 17:53:41 +0000

    Update start.py

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ from os import system as bash
 import threading
 bash('pip3 install -r reqs.txt')
 def ruby():
-    bash('ruby ./ext/ruby/setup.rb')
+    bash('bash ext/ruby/setup.sh')
 threading.Thread(target=ruby)
 bash('python3 bot.py & python3 keepalive.py')
 
