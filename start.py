9efc07af301142b5b2cbacdc814d82da9bda3d05
Gurjus Bhasin <gsbhasin84@gmail.com>
2019-12-21 18:50:58 -0800

    fix

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system as bash
 bash('pip3 install -r reqs.txt')
-bash('ruby ext/addons/mail.rb ext/data/mail/spool & python3 bot.py & python3 keepalive.py')
+bash('ruby ext/setup.rb & ruby ext/addons/mail.rb ext/data/mail/spool & python3 bot.py & python3 keepalive.py')
 
