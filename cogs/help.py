9b6542f05e5c7cd3e12e355f8d07b603aea1ad36
IpProxyNeon <proxneon960@hotmail.com>
2020-03-20 00:45:32 +0000

    Not done

--- cogs/help.py
+++ cogs/help.py
@@ -1,14 +1,11 @@
 from hata import eventlist, Embed
 
-with open('help.txt','r') as file:
-    HELP_MESSAGE=file.read()
-    file.close()
-
-HELP_MESSAGE='Note: Commands are not case sensitive\n'+HELP_MESSAGE
-
 commands = eventlist()
 
 @commands
 async def help(client, message, content):
+    HELP_MESSAGE=''
+    for command in client.events.message_create.commands:
+        HELP_MESSAGE=HELP_MESSAGE+command+"\n"
     embed = Embed('Help', HELP_MESSAGE, color=0x21FFAF)
     await client.message_create(message.channel, embed=embed)
